017050c240f08fccaa863a3b9c3eba514793588b
f-tominaga <tommy.tommy.walker@gmail.com>
2022-03-03 12:37:28 +0900

    add download pth

---
+++ download_pth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gdrive_download 18lDUHl_-QlAQIZJxfcyUwcstmZhEC0DX htc_r50_fpn_1x_TACO_20210302_epoch2200.pth /mmdetection/checkpoints
--- gdrive_download.sh
+++ gdrive_download.sh
@@ -2,6 +2,13 @@
 # Usage: gdrive_download 123-abc ./output.zip
 function gdrive_download () { 
   CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$1" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')
-  wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2 -P $3
   rm -f /tmp/cookies.txt
 }
+
+# echo 'ID =' "$1";
+# echo 'NAME = ' "$2";
+# CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$S1" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
+# echo 'CONFIRM =' "$CONFIRM";
+# wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$S1" -O $S2 -P $3;
+# rm -f /tmp/cookies.txt
