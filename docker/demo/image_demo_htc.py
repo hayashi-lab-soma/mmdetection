7255598def03f79f66d3f55f9dd119d520085ba5
f-tominaga <tommy.tommy.walker@gmail.com>
2022-03-03 13:39:54 +0900

    build image_demo

---
+++ docker/demo/image_demo_htc.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser
+from mmdet.apis import (async_inference_detector, inference_detector,
+                        init_detector, show_result_pyplot)
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('img', help='Image file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--palette',
+        default='coco',
+        choices=['coco', 'voc', 'citys', 'random'],
+        help='Color palette used for visualization')
+    parser.add_argument(
+        '--score-thr', type=float, default=0.3, help='bbox score threshold')
+    parser.add_argument(
+        '--async-test',
+        action='store_true',
+        help='whether to set async options for async inference.')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+  args = parse_args()
+
+  # build the model from a config file and a checkpoint file
+  model = init_detector(args.config, args.checkpoint, device=args.device)
